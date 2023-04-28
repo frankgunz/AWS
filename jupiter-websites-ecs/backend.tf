6d2aa22a966e5a82644bc55beece5959e0326166
Tsopmo <frank.tsopmo@gecurrent.com>
2023-04-27 22:50:09 -0500

    Initial commit

---
+++ jupiter-websites-ecs/backend.tf
@@ -0,0 +1,9 @@
+# store the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket    = "geterraform"
+    key       = "jupiter_website-ecs.tfstate"
+    region    = "us-east-1"
+    profile   = "terraform-user"
+  }
+}
